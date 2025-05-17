51bc8295d6df74cdb936355ee42a61089d95adee
Matias de Andrea <deandreamatias@gmail.com>
2025-05-17 17:44:33 +0200

    build: setup new platforms

--- example/android/app/src/main/kotlin/com/example/form_builder_phone_field/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.form_builder_phone_field
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ example/android/app/src/main/kotlin/com/flutterformbuilderecosystem/example/example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.flutterformbuilderecosystem.example.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
